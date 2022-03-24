89f066dc1ec893a99a8da0839df006210b692e35
RobyPurbaSakty26 <obykao26@gmail.com>
2022-03-24 14:42:18 +0700

    menambahkan kommit

---
+++ test/01-fungsi/fungsi_dengan_parameter.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  greet('Docoding', 2015);
+}
+
+void greet(String name, bornYear){
+  var age = 2020 -bornYear;
+  print('Hallo $name! Tahun ini anda berusia $age tahun');
+}
---
+++ test/01-fungsi/main.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  greet();
+}
+
+void greet(){
+  print('Hello');
+}
