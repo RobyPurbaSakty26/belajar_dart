e6b60c2077abc54b09589ec22645b8cf068e85b8
RobyPurbaSakty26 <obykao26@gmail.com>
2022-04-01 11:51:50 +0700

    for each

---
+++ test/03-pfp/for_each.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  final arr = <int>[1, 2, 3, 4, 5, 6];
+
+  arr.forEach(print);
+}
---
+++ test/03-pfp/test.dart
@@ -0,0 +1,3 @@
+void main(List<String> args) {
+  var companyData = <num, String>{32: '203'};
+}
